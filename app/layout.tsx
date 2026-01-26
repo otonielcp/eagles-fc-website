f97678e95d0b628246b428b51f1d39af5e4f124e
Otoniel Cortez <68568965+otonielcp@users.noreply.github.com>
2026-01-26 13:22:25 -0600

    Fix hydration mismatch warning by adding suppressHydrationWarning to html and body tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,12 +26,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
         {children}
       </body>
-    </html >
+    </html>
   );
 }
